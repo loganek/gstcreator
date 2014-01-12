137fd87c0c311228711f39153b347de2ff96f474
Marcin Kolny <marcin.kolny@gmail.com>
2014-01-12 15:29:16 +0100

    added main file, travis file

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/*
+ * gstcreator
+ *     main.cpp
+ *
+ *  Created on: 12 sty 2014
+ *     Author: Marcin Kolny <marcin.kolny@gmail.com>
+ */
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
+
