0e98c578728cde0f8fb4e383971acce1c101da3a
Marcin Kolny <marcin.kolny@gmail.com>
2014-02-14 18:12:59 +0100

    setting child state from parent

--- src/Commands/AddCommand.cpp
+++ src/Commands/AddCommand.cpp
@@ -24,7 +24,12 @@ void AddCommand::run_command()
 		if (!model->is_bin())
 			throw std::runtime_error("parent object must be a BIN object!");
 
-		RefPtr<Bin>::cast_static(model)->add(RefPtr<Element>::cast_static(item));
+		auto new_element = RefPtr<Element>::cast_static(item);
+		auto parent = RefPtr<Bin>::cast_static(model);
+		parent->add(new_element);
+		State state, pending;
+		parent->get_state(state, pending, 0);
+		new_element->set_state(state);
 	}
 
 	else if (item->is_pad() && model->is_element())
