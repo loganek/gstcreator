df8ad5c816244c9c82b89a6025ece142b4a18fa7
Marcin Kolny <marcin.kolny@gmail.com>
2014-02-15 01:38:11 +0100

    default implementation in IModelObserver

--- src/Logic/include/Logic/IModelObserver.h
+++ src/Logic/include/Logic/IModelObserver.h
@@ -14,12 +14,12 @@ class IModelObserver
 {
 public:
 	virtual ~IModelObserver(){}
-	virtual void pad_added(const Glib::RefPtr<Gst::Pad>& pad) = 0;
-	virtual void pad_removed(const Glib::RefPtr<Gst::Pad>& pad) = 0;
-	virtual void pad_linked(const Glib::RefPtr<Gst::Pad>& proxy_pad) = 0;
-	virtual void pad_unlinked(const Glib::RefPtr<Gst::Pad>& proxy_pad) = 0;
-	virtual void element_added(const Glib::RefPtr<Gst::Element>& element) = 0;
-	virtual void element_removed(const Glib::RefPtr<Gst::Element>& element) = 0;
+	virtual void pad_added(const Glib::RefPtr<Gst::Pad>& pad) {}
+	virtual void pad_removed(const Glib::RefPtr<Gst::Pad>& pad) {}
+	virtual void pad_linked(const Glib::RefPtr<Gst::Pad>& proxy_pad) {}
+	virtual void pad_unlinked(const Glib::RefPtr<Gst::Pad>& proxy_pad) {}
+	virtual void element_added(const Glib::RefPtr<Gst::Element>& element) {}
+	virtual void element_removed(const Glib::RefPtr<Gst::Element>& element) {}
 
 };
 
